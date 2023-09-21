20c7cfbf7d2e187e73fd89cb0cd9fd9a63009164
sophiexxs <sofiafamulak@gmail.com>
2023-09-21 21:35:14 +0300

    test

--- MoveZeros.py
+++ MoveZeros.py
@@ -3,3 +3,8 @@ n_of_zeros = nums.count(0)
 while 0 in nums:
     nums.remove(0)
 nums += n_of_zeros * [0] #ghgjhbjhdsfdfds
+nums = [0, 1, 0, 3, 12]
+n_of_zeros = nums.count(0)
+while 0 in nums:
+    nums.remove(0)
+nums += n_of_zeros * [0] #ghgjhbjhdsfdfds
